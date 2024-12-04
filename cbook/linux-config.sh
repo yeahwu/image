a4bfa78ba0fa7720dfbf3ccb16a987193b5bbf46
yeahwu <yeahwu@users.noreply.github.com>
2024-12-04 13:04:41 +0800

    Update linux-config.sh

--- cbook/linux-config.sh
+++ cbook/linux-config.sh
@@ -123,7 +123,7 @@ sed -ri 's/^#?PubkeyAuthentication.*/PubkeyAuthentication yes/g' /etc/ssh/sshd_c
 sed -ri 's/^#?AuthorizedKeysFile.*/AuthorizedKeysFile .ssh\/authorized_keys .ssh\/authorized_keys2/g' /etc/ssh/sshd_config;
 
 # dns resolv.conf
-# echo -e "nameserver 8.8.8.8\nsearch ." >/etc/resolv.conf
+echo -e "nameserver 8.8.8.8\nsearch ." >/etc/resolv.conf
 
 # remove excess
 apt purge nftables apache2 fail2ban -y
